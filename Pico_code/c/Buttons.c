c3484ccea4e327fd93cf31fb547e42084c205143
ruizmic0100 <ruizmic0100@hotmail.com>
2023-03-25 16:20:16 -0500

    Gave you an idea of what to do

---
+++ Pico_code/c/Buttons.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/** Input Pet
+ * 
+ * TODO: Pet the animal passed in.
+*/
+int Pet_Button(Pet dog)
+{
+    // dog.petted
+}
+
+// type function-name(inputs)
+// return type
+int main()
+{
+    return 0;
+}
