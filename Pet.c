d2d2ed526acff38ef3a91e10345dfb4a18e16094
Hamza <hamza>
2023-03-25 17:52:05 -0500

    energy_>stamina

---
+++ Pet.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "Pet.h"
+
+Pet makeHappy(Pet pet){
+    pet.mood = happy;
+    return pet;
+}
+Pet makeSad(Pet pet){
+    pet.mood = sad;
+    return pet;
+}
+Pet makeNeutral(Pet pet){
+    pet.mood = neutral;
+    return pet;
+}
+Pet sleep(Pet pet){
+    pet.stamina = 100;
+    return pet;
+}
+Pet feed(Pet pet){
+    pet.hunger += 25;
+    if(pet.hunger > 100){
+        pet.hunger = 100;
+    }
+
+
+    // code
+    return pet;
+}
+Pet walk(Pet pet){
+    pet.stamina -= 25;
+    if(pet.stamina < 0){
+        pet.stamina = 0;
+    }
+    return pet;
+}
